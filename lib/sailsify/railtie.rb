1b5d1ed6644a1f162d23bba0011aab6cf42da60a
colin <acolin@incaztech.com>
2015-11-14 23:35:48 -0600

    removed eager load form railtie

--- lib/sailsify/railtie.rb
+++ lib/sailsify/railtie.rb
@@ -4,9 +4,9 @@ module Sailsify
       Sailsify.destination_dir = app.root.join('sailsify')
     end
 
-    initializer 'sailsify.load_descentands' do |app|
-      app.eager_load!
-    end
+    #initializer 'sailsify.load_descentands' do |app|
+      #app.eager_load!
+    #end
 
     rake_tasks do
       load 'tasks/sailsify_tasks.rake'
